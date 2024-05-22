216cd0850d170c4b25c7b28637043fce934f7330
Adnan-Eram-Argho <adnaneramargho@gmail.com>
2024-05-22 17:41:42 +0600

    secured

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVrxLgOhDWyTZhttAMRouFXpD0mLNlm4c",
-  authDomain: "recipe-app-130b1.firebaseapp.com",
-  projectId: "recipe-app-130b1",
-  storageBucket: "recipe-app-130b1.appspot.com",
-  messagingSenderId: "839865015265",
-  appId: "1:839865015265:web:9403ea9c64193b714b10a0"
+  apiKey: import.meta.env.Vite_apiKey,
+  authDomain:import.meta.env.Vite_authDomain,
+  projectId: import.meta.env.Vite_projectId,
+  storageBucket:import.meta.env.Vite_storageBucket,
+  messagingSenderId: import.meta.env.Vite_messagingSenderId,
+  appId: import.meta.env.Vite_appId,
 };
 
 // Initialize Firebase
